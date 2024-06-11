ff96612291a3c167abb73f25f3bebbfd8c80aa04
GuilhermeJWT <guiromanno@gmail.com>
2024-06-11 08:24:35 -0300

    ADD: projeto

---
+++ src/main/java/br/com/systemsgs/ordem_servico_backend/OrdemServicoBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.ordem_servico_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemServicoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemServicoBackendApplication.class, args);
+	}
+
+}
