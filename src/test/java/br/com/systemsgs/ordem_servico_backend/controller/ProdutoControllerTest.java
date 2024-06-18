1c7dc542fe7750d05c1f20510d4799b6d5a97ddb
GuilhermeJWT <guiromanno@gmail.com>
2024-06-18 14:58:47 -0300

    ADD: mais testes de integração - Produtos

--- src/test/java/br/com/systemsgs/ordem_servico_backend/controller/ProdutoControllerTest.java
+++ src/test/java/br/com/systemsgs/ordem_servico_backend/controller/ProdutoControllerTest.java
@@ -1,7 +1,9 @@
 package br.com.systemsgs.ordem_servico_backend.controller;
 
 import br.com.systemsgs.ordem_servico_backend.dto.ModelProdutosDTO;
+import br.com.systemsgs.ordem_servico_backend.exception.RecursoNaoEncontradoException;
 import br.com.systemsgs.ordem_servico_backend.model.ModelProdutos;
+import br.com.systemsgs.ordem_servico_backend.repository.ProdutoRepository;
 import br.com.systemsgs.ordem_servico_backend.service.ProdutoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +23,9 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 @SpringBootTest
 class ProdutoControllerTest {
@@ -44,8 +49,11 @@ class ProdutoControllerTest {
     private ProdutoService produtoService;
 
     @Mock
-    private ModelMapper mapper;
+    private ProdutoRepository produtoRepository;
 
+    @Mock
+    private ModelMapper mapper;
+    
     @BeforeEach
     void setUp(){
         MockitoAnnotations.openMocks(this);
@@ -97,6 +105,66 @@ class ProdutoControllerTest {
         assertEquals(PRECO_COMPRA, response.getBody().getPreco_compra());
         assertEquals(PRECO_VENDA, response.getBody().getPreco_venda());
         assertEquals(CODIGO_BARRAS, response.getBody().getCodigo_barras());
+    }
+
+    @DisplayName("Pesquisa um Produto inexistente e retorna 404")
+    @Test
+    void pesquisaProdutoRetorna404(){
+        when(produtoRepository.findById(anyLong())).thenThrow(new RecursoNaoEncontradoException());
+
+        try{
+            produtoService.pesquisaPorId(ID);
+        }catch (Exception exception){
+            assertEquals(RecursoNaoEncontradoException.class, exception.getClass());
+            assertEquals("Recurso não Encontrado!", exception.getMessage());
+        }
+    }
+
+    @DisplayName("Salva um Produto e retorna 201")
+    @Test
+    void salvaProdutoRetorna201(){
+        when(produtoService.salvarProdutos(any())).thenReturn(modelProdutos);
+
+        ResponseEntity<ModelProdutosDTO> response = produtoController.salvarProduto(modelProdutosDTO);
+
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getHeaders().get("Location"));
+    }
+
+    @DisplayName("Atualiza um Produto e retorna 200")
+    @Test
+    void atualizaProdutoRetorna200(){
+        when(produtoService.atualizarProduto(ID,modelProdutosDTO)).thenReturn(modelProdutos);
+        when(mapper.map(any(), any())).thenReturn(modelProdutosDTO);
+
+        ResponseEntity<ModelProdutosDTO> response = produtoController.atualizarProdutos(ID, modelProdutosDTO);
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ModelProdutosDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(DESCRICAO, response.getBody().getDescricao());
+        assertEquals(QUANTIDADE, response.getBody().getQuantidade());
+        assertEquals(QUANTIDADE_MINIMA, response.getBody().getQuantidade_minima());
+        assertEquals(PRECO_COMPRA, response.getBody().getPreco_compra());
+        assertEquals(PRECO_VENDA, response.getBody().getPreco_venda());
+        assertEquals(CODIGO_BARRAS, response.getBody().getCodigo_barras());
+    }
+
+    @DisplayName("Deleta um Produto e retorna 204")
+    @Test
+    void deletaProdutoRetorna204(){
+        doNothing().when(produtoService).deletarProduto(anyLong());
+
+        ResponseEntity<ModelProdutosDTO> response = produtoController.delete(ID);
+
+        assertNotNull(response);
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        verify(produtoService, times(1)).deletarProduto(anyLong());
     }
 
     private void startProduto(){
