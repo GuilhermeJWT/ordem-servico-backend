b6743bd6c36d316e7ea7dfb28ece3a9b7e8400e7
GuilhermeJWT <guiromanno@gmail.com>
2024-06-21 16:17:00 -0300

    UPDATE: Refatorando a classe OrdemServicoControllerTest

--- src/test/java/br/com/systemsgs/ordem_servico_backend/controller/OrdemServicoControllerTest.java
+++ src/test/java/br/com/systemsgs/ordem_servico_backend/controller/OrdemServicoControllerTest.java
@@ -1,9 +1,8 @@
 package br.com.systemsgs.ordem_servico_backend.controller;
 
+import br.com.systemsgs.ordem_servico_backend.ConfigDadosEstaticosEntidades;
 import br.com.systemsgs.ordem_servico_backend.dto.ModelOrdemServicoDTO;
-import br.com.systemsgs.ordem_servico_backend.enums.Status;
 import br.com.systemsgs.ordem_servico_backend.exception.RecursoNaoEncontradoException;
-import br.com.systemsgs.ordem_servico_backend.model.ModelClientes;
 import br.com.systemsgs.ordem_servico_backend.model.ModelOrdemServico;
 import br.com.systemsgs.ordem_servico_backend.repository.OrdemServicoRepository;
 import br.com.systemsgs.ordem_servico_backend.service.OrdemServicoService;
@@ -19,7 +18,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,18 +29,10 @@ import static org.mockito.Mockito.times;
 @SpringBootTest
 class OrdemServicoControllerTest {
 
-    public static final Long ID = 1L;
-    public static final String DEFEITO = "Trocar a tela";
-    public static final String DESCRICAO = "Descricao OS";
-    public static final String LAUDO_TECNICO = "Precisa trocar a tela";
-    public static final Status STATUS = Status.ORCAMENTO;
-    public static final Date DATA_INICIAL = new Date();
-    public static final Date DATA_FINAL = new Date();
-    public static final ModelClientes CLIENTE = new ModelClientes();
-    public static final Integer INDEX_0 = 0;
-
     private ModelOrdemServico modelOrdemServico;
     private ModelOrdemServicoDTO modelOrdemServicoDTO;
+
+    private ConfigDadosEstaticosEntidades getDadosEstaticosOS = new ConfigDadosEstaticosEntidades();
 
     @InjectMocks
     private OrdemServicoController ordemServicoController;
@@ -75,16 +65,25 @@ class OrdemServicoControllerTest {
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(ResponseEntity.class, response.getClass());
         assertEquals(ArrayList.class, response.getBody().getClass());
-        assertEquals(ModelOrdemServicoDTO.class, response.getBody().get(INDEX_0).getClass());
+        assertEquals(ModelOrdemServicoDTO.class, response.getBody().get(0).getClass());
 
-        assertEquals(ID, response.getBody().get(INDEX_0).getId());
-        assertEquals(DEFEITO, response.getBody().get(INDEX_0).getDefeito());
-        assertEquals(DESCRICAO, response.getBody().get(INDEX_0).getDescricao());
-        assertEquals(LAUDO_TECNICO, response.getBody().get(INDEX_0).getLaudo_tecnico());
-        assertEquals(STATUS, response.getBody().get(INDEX_0).getStatus());
-        assertEquals(DATA_INICIAL, response.getBody().get(INDEX_0).getData_inicial());
-        assertEquals(DATA_FINAL, response.getBody().get(INDEX_0).getData_final());
-        assertEquals(CLIENTE, response.getBody().get(INDEX_0).getCliente());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getId(), response.getBody().get(0).getId());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getDefeito(), response.getBody().get(0).getDefeito());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getDescricao(), response.getBody().get(0).getDescricao());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getLaudo_tecnico(), response.getBody().get(0).getLaudo_tecnico());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getStatus(), response.getBody().get(0).getStatus());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getData_inicial(), response.getBody().get(0).getData_inicial());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getData_final(), response.getBody().get(0).getData_final());
+
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getId(), response.getBody().get(0).getCliente().getId());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getNome(), response.getBody().get(0).getCliente().getNome());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getCpf(), response.getBody().get(0).getCliente().getCpf());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getCelular(), response.getBody().get(0).getCliente().getCelular());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getEmail(), response.getBody().get(0).getCliente().getEmail());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getEndereco(), response.getBody().get(0).getCliente().getEndereco());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getCidade(), response.getBody().get(0).getCliente().getCidade());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getEstado(), response.getBody().get(0).getCliente().getEstado());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getCep(), response.getBody().get(0).getCliente().getCep());
     }
 
     @DisplayName("Pesquisa uma OS pelo ID e testa o Retorno do Body - retorna 200")
@@ -93,7 +92,8 @@ class OrdemServicoControllerTest {
         when(ordemServicoService.listarOS()).thenReturn(List.of(modelOrdemServico));
         when(mapper.map(any(), any())).thenReturn(modelOrdemServicoDTO);
 
-        ResponseEntity<ModelOrdemServicoDTO> response = ordemServicoController.pesquisarPorId(ID);
+        ResponseEntity<ModelOrdemServicoDTO> response = ordemServicoController.
+                pesquisarPorId(getDadosEstaticosOS.dadosOrdemServico().getId());
 
         assertNotNull(response);
         assertNotNull(response.getBody());
@@ -101,14 +101,23 @@ class OrdemServicoControllerTest {
         assertEquals(ResponseEntity.class, response.getClass());
         assertEquals(ModelOrdemServicoDTO.class, response.getBody().getClass());
 
-        assertEquals(ID, response.getBody().getId());
-        assertEquals(DEFEITO, response.getBody().getDefeito());
-        assertEquals(DESCRICAO, response.getBody().getDescricao());
-        assertEquals(LAUDO_TECNICO, response.getBody().getLaudo_tecnico());
-        assertEquals(STATUS, response.getBody().getStatus());
-        assertEquals(DATA_INICIAL, response.getBody().getData_inicial());
-        assertEquals(DATA_FINAL, response.getBody().getData_final());
-        assertEquals(CLIENTE, response.getBody().getCliente());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getId(), response.getBody().getId());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getDefeito(), response.getBody().getDefeito());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getDescricao(), response.getBody().getDescricao());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getLaudo_tecnico(), response.getBody().getLaudo_tecnico());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getStatus(), response.getBody().getStatus());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getData_inicial(), response.getBody().getData_inicial());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getData_final(), response.getBody().getData_final());
+
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getId(), response.getBody().getCliente().getId());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getNome(), response.getBody().getCliente().getNome());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getCpf(), response.getBody().getCliente().getCpf());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getCelular(), response.getBody().getCliente().getCelular());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getEmail(), response.getBody().getCliente().getEmail());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getEndereco(), response.getBody().getCliente().getEndereco());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getCidade(), response.getBody().getCliente().getCidade());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getEstado(), response.getBody().getCliente().getEstado());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getCep(), response.getBody().getCliente().getCep());
     }
 
     @DisplayName("Pesquisa uma OS inexistente e retorna 404")
@@ -117,7 +126,7 @@ class OrdemServicoControllerTest {
         when(ordemServicoRepository.findById(anyLong())).thenThrow(new RecursoNaoEncontradoException());
 
         try{
-            ordemServicoService.pesquisaPorId(ID);
+            ordemServicoService.pesquisaPorId(getDadosEstaticosOS.dadosOrdemServico().getId());
         }catch (Exception exception){
             assertEquals(RecursoNaoEncontradoException.class, exception.getClass());
             assertEquals("Recurso não Encontrado!", exception.getMessage());
@@ -139,24 +148,37 @@ class OrdemServicoControllerTest {
     @DisplayName("Atualiza uma OS e retorna 200")
     @Test
     void atualizaOsRetorna200(){
-        when(ordemServicoService.atualizarOS(ID,modelOrdemServicoDTO)).thenReturn(modelOrdemServico);
+        when(ordemServicoService.atualizarOS(
+                getDadosEstaticosOS.dadosOrdemServico().getId(),modelOrdemServicoDTO)).thenReturn(modelOrdemServico);
+
         when(mapper.map(any(), any())).thenReturn(modelOrdemServicoDTO);
 
-        ResponseEntity<ModelOrdemServicoDTO> response = ordemServicoController.atualizarOS(ID, modelOrdemServicoDTO);
+        ResponseEntity<ModelOrdemServicoDTO> response = ordemServicoController.
+                atualizarOS(getDadosEstaticosOS.dadosOrdemServico().getId(), modelOrdemServicoDTO);
+
         assertNotNull(response);
         assertNotNull(response.getBody());
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(ResponseEntity.class, response.getClass());
         assertEquals(ModelOrdemServicoDTO.class, response.getBody().getClass());
 
-        assertEquals(ID, response.getBody().getId());
-        assertEquals(DEFEITO, response.getBody().getDefeito());
-        assertEquals(DESCRICAO, response.getBody().getDescricao());
-        assertEquals(LAUDO_TECNICO, response.getBody().getLaudo_tecnico());
-        assertEquals(STATUS, response.getBody().getStatus());
-        assertEquals(DATA_INICIAL, response.getBody().getData_inicial());
-        assertEquals(DATA_FINAL, response.getBody().getData_final());
-        assertEquals(CLIENTE, response.getBody().getCliente());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getId(), response.getBody().getId());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getDefeito(), response.getBody().getDefeito());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getDescricao(), response.getBody().getDescricao());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getLaudo_tecnico(), response.getBody().getLaudo_tecnico());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getStatus(), response.getBody().getStatus());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getData_inicial(), response.getBody().getData_inicial());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getData_final(), response.getBody().getData_final());
+
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getId(), response.getBody().getCliente().getId());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getNome(), response.getBody().getCliente().getNome());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getCpf(), response.getBody().getCliente().getCpf());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getCelular(), response.getBody().getCliente().getCelular());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getEmail(), response.getBody().getCliente().getEmail());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getEndereco(), response.getBody().getCliente().getEndereco());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getCidade(), response.getBody().getCliente().getCidade());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getEstado(), response.getBody().getCliente().getEstado());
+        assertEquals(getDadosEstaticosOS.dadosOrdemServico().getCliente().getCep(), response.getBody().getCliente().getCep());
     }
 
     @DisplayName("Deleta uma OS e retorna 204")
@@ -164,7 +186,7 @@ class OrdemServicoControllerTest {
     void deletaOsRetorna204(){
         doNothing().when(ordemServicoService).deletarOS(anyLong());
 
-        ResponseEntity<ModelOrdemServicoDTO> response = ordemServicoController.deletarOS(ID);
+        ResponseEntity<ModelOrdemServicoDTO> response = ordemServicoController.deletarOS(getDadosEstaticosOS.dadosOrdemServico().getId());
 
         assertNotNull(response);
         assertEquals(ResponseEntity.class, response.getClass());
@@ -173,8 +195,25 @@ class OrdemServicoControllerTest {
     }
 
     private void startOrdemServico(){
-        modelOrdemServico = new ModelOrdemServico(ID, DEFEITO, DESCRICAO, LAUDO_TECNICO, STATUS, DATA_INICIAL, DATA_FINAL, CLIENTE);
-        modelOrdemServicoDTO = new ModelOrdemServicoDTO(ID, DEFEITO, DESCRICAO, LAUDO_TECNICO, STATUS, DATA_INICIAL, DATA_FINAL, CLIENTE);
+        modelOrdemServico = new ModelOrdemServico(
+                getDadosEstaticosOS.dadosOrdemServico().getId(),
+                getDadosEstaticosOS.dadosOrdemServico().getDefeito(),
+                getDadosEstaticosOS.dadosOrdemServico().getDescricao(),
+                getDadosEstaticosOS.dadosOrdemServico().getLaudo_tecnico(),
+                getDadosEstaticosOS.dadosOrdemServico().getStatus(),
+                getDadosEstaticosOS.dadosOrdemServico().getData_inicial(),
+                getDadosEstaticosOS.dadosOrdemServico().getData_final(),
+                getDadosEstaticosOS.dadosOrdemServico().getCliente()
+        );
+        modelOrdemServicoDTO = new ModelOrdemServicoDTO(
+                getDadosEstaticosOS.dadosOrdemServico().getId(),
+                getDadosEstaticosOS.dadosOrdemServico().getDefeito(),
+                getDadosEstaticosOS.dadosOrdemServico().getDescricao(),
+                getDadosEstaticosOS.dadosOrdemServico().getLaudo_tecnico(),
+                getDadosEstaticosOS.dadosOrdemServico().getStatus(),
+                getDadosEstaticosOS.dadosOrdemServico().getData_inicial(),
+                getDadosEstaticosOS.dadosOrdemServico().getData_final(),
+                getDadosEstaticosOS.dadosOrdemServico().getCliente()
+        );
     }
-
 }
