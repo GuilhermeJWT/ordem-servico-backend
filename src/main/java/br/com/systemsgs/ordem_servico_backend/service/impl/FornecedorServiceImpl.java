413b07bbf650dc8e18c0ce09936ed5918d7ada8d
GuilhermeJWT <guiromanno@gmail.com>
2024-07-19 13:14:05 -0300

    ADD: Camada DTO, Service e Exception Handler

---
+++ src/main/java/br/com/systemsgs/ordem_servico_backend/service/impl/FornecedorServiceImpl.java
@@ -0,0 +1,62 @@
+package br.com.systemsgs.ordem_servico_backend.service.impl;
+
+import br.com.systemsgs.ordem_servico_backend.dto.ModelFornecedorDTO;
+import br.com.systemsgs.ordem_servico_backend.exception.FornecedorNaoEncontradoException;
+import br.com.systemsgs.ordem_servico_backend.model.ModelFornecedor;
+import br.com.systemsgs.ordem_servico_backend.repository.FornecedoresRepository;
+import br.com.systemsgs.ordem_servico_backend.service.FornecedorService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FornecedorServiceImpl implements FornecedorService {
+
+    @Autowired
+    private FornecedoresRepository fornecedoresRepository;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Override
+    public ModelFornecedor pesquisaPorId(Long id) {
+        Optional<ModelFornecedor> modelFornecedor = fornecedoresRepository.findById(id);
+        return modelFornecedor.orElseThrow(() -> new FornecedorNaoEncontradoException());
+    }
+
+    @Override
+    public List<ModelFornecedor> listarFornecedores() {
+        return fornecedoresRepository.findAll();
+    }
+
+    @Override
+    public ModelFornecedor salvarFornecedor(ModelFornecedorDTO modelFornecedorDTO) {
+        ModelFornecedor fornecedorConvertido = mapper.map(modelFornecedorDTO, ModelFornecedor.class);
+        return fornecedoresRepository.save(fornecedorConvertido);
+    }
+
+    @Override
+    public void deletarFornecedor(Long id) {
+        fornecedoresRepository.deleteById(id);
+    }
+
+    @Override
+    public ModelFornecedor updateFornecedor(Long id, ModelFornecedorDTO modelFornecedorDTO) {
+        ModelFornecedor fornecedorPesquisado = pesquisaFornecedorPeloId(id);
+        mapper.map(modelFornecedorDTO, ModelFornecedor.class);
+        BeanUtils.copyProperties(modelFornecedorDTO, fornecedorPesquisado, "id");
+
+        return fornecedoresRepository.save(fornecedorPesquisado);
+    }
+
+    public ModelFornecedor pesquisaFornecedorPeloId(Long id){
+       ModelFornecedor pesquisaFornecedor = fornecedoresRepository.findById(id)
+               .orElseThrow(() -> new FornecedorNaoEncontradoException());
+
+       return pesquisaFornecedor;
+    }
+}
