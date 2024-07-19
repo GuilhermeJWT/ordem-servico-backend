4be7f08066cd2d8b46f7bc4502dcce5032959d7e
GuilhermeJWT <guiromanno@gmail.com>
2024-07-19 15:50:26 -0300

    ADD: Model de Contas a Pagar

---
+++ src/main/java/br/com/systemsgs/ordem_servico_backend/enums/FormaPagamento.java
@@ -0,0 +1,15 @@
+package br.com.systemsgs.ordem_servico_backend.enums;
+
+public enum FormaPagamento {
+
+    DINHEIRO,
+    CARTAO_CREDITO,
+    CARTAO_DEBITO,
+    BOLETO,
+    PIX,
+    TRANSFERENCIA_BANCARIA,
+    CARTEIRA_DIGITAL,
+    CHEQUE,
+    OUTRO
+
+}
---
+++ src/main/java/br/com/systemsgs/ordem_servico_backend/enums/StatusContasPagar.java
@@ -0,0 +1,16 @@
+package br.com.systemsgs.ordem_servico_backend.enums;
+
+public enum StatusContasPagar {
+
+    EM_ABERTO,
+    PENDENTE,
+    CONFIRMADA,
+    BAIXADA,
+    ATRASADA,
+    PAUSADA,
+    CANCELADA,
+    EXPIRADA,
+    INADINPLENTE,
+    AGUARDANDO_REEMBOLSO
+
+}
