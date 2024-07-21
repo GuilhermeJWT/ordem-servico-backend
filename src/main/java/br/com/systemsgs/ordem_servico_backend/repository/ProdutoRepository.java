f21f66dccc8cc369af6ac7db2a6f3a8fcee47b84
GuilhermeJWT <guiromanno@gmail.com>
2024-07-21 20:54:01 -0300

    ADD: Criando as consultas para o Dashboard

--- src/main/java/br/com/systemsgs/ordem_servico_backend/repository/ProdutoRepository.java
+++ src/main/java/br/com/systemsgs/ordem_servico_backend/repository/ProdutoRepository.java
@@ -2,9 +2,14 @@ package br.com.systemsgs.ordem_servico_backend.repository;
 
 import br.com.systemsgs.ordem_servico_backend.model.ModelProdutos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<ModelProdutos, Long> {
 
+    @Query(value = "SELECT SUM(q.quantidade) FROM tbl_produtos q", nativeQuery = true)
+    Optional<Integer> somaEstoqueAtual();
 }
