4f4866c601fb2d06973f676201b0cc0f874896fb
GuilhermeJWT <guiromanno@gmail.com>
2024-07-22 10:32:11 -0300

    ADD: Exception para método Http incorreto

--- src/main/java/br/com/systemsgs/ordem_servico_backend/exception/ExceptionControllerAdvice.java
+++ src/main/java/br/com/systemsgs/ordem_servico_backend/exception/ExceptionControllerAdvice.java
@@ -4,6 +4,7 @@ import br.com.systemsgs.ordem_servico_backend.exception.errors.*;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -41,6 +42,12 @@ public class ExceptionControllerAdvice {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiRestErrors internalServerErroException(){
         return new ApiRestErrors(new ErroInternoException().getMessage());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiRestErrors metodoHttpNaoSuportadoException(){
+        return new ApiRestErrors(new MetodoHttpNaoSuportadoException().getMessage());
     }
 
     @ExceptionHandler(TokenInvalidoException.class)
