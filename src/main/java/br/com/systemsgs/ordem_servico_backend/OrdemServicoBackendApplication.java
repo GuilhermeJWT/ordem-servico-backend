c00c2e4d6a47fd940983e7b8270e38be3bd2373b
GuilhermeJWT <guiromanno@gmail.com>
2024-07-22 18:25:49 -0300

    ADD: Habilitando expiração de 1 hora para os Caches

--- src/main/java/br/com/systemsgs/ordem_servico_backend/OrdemServicoBackendApplication.java
+++ src/main/java/br/com/systemsgs/ordem_servico_backend/OrdemServicoBackendApplication.java
@@ -3,8 +3,10 @@ package br.com.systemsgs.ordem_servico_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableCaching
 public class OrdemServicoBackendApplication {
 
---
+++ src/main/java/br/com/systemsgs/ordem_servico_backend/config/CacheExpireConfiguration.java
@@ -0,0 +1,26 @@
+package br.com.systemsgs.ordem_servico_backend.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CacheExpireConfiguration {
+
+    @Autowired
+    public CacheManager cacheManager;
+
+    public void limpandoCaches() {
+        cacheManager.getCacheNames().stream()
+                .forEach(cacheName -> cacheManager.getCache(cacheName).clear());
+    }
+
+    @Scheduled(fixedRate = 3600000) // 1 Hora
+    public void jobCache() {
+        limpandoCaches();
+        log.info("Limpando os Caches da aplicação...");
+    }
+}
