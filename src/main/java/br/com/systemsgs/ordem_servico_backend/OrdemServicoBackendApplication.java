6f73714a0d1a3e1f70ce359c6759185212819db9
GuilhermeJWT <guiromanno@gmail.com>
2024-07-22 17:53:07 -0300

    ADD: Habilitando Cache na Aplicação

--- src/main/java/br/com/systemsgs/ordem_servico_backend/OrdemServicoBackendApplication.java
+++ src/main/java/br/com/systemsgs/ordem_servico_backend/OrdemServicoBackendApplication.java
@@ -2,8 +2,10 @@ package br.com.systemsgs.ordem_servico_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class OrdemServicoBackendApplication {
 
 	public static void main(String[] args) {
