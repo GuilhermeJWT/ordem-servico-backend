0623b5161b65d953f0572fb635edae6882872a08
GuilhermeJWT <guiromanno@gmail.com>
2024-07-26 01:07:35 -0300

    ADD: Teste - UtilProdutos

--- src/test/java/br/com/systemsgs/ordem_servico_backend/util/UtilProdutosTest.java
+++ src/test/java/br/com/systemsgs/ordem_servico_backend/util/UtilProdutosTest.java
@@ -13,6 +13,8 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,6 +26,7 @@ import static org.mockito.Mockito.when;
 class UtilProdutosTest extends ConfigDadosEstaticosEntidades{
 
     private Optional<ModelProdutos> modelProdutosOptional;
+    private List<ModelProdutos> listModelProdutos;
 
     @InjectMocks
     private UtilProdutos utilProdutos;
@@ -68,6 +71,61 @@ class UtilProdutosTest extends ConfigDadosEstaticosEntidades{
         }
     }
 
+    @DisplayName("Pesquisa lista de Produtos com uma lista de IDs")
+    @Test
+    void testPesquisaListaProdutosPorIds(){
+        when(produtoRepository.findAllById(dadosIds())).thenReturn(listModelProdutos);
+
+        List<ModelProdutos> response = utilProdutos.pesquisaListaProdutosPorIds(dadosIds());
+
+        assertNotNull(response);
+
+        assertEquals(dadosProdutos().getId(), response.get(0).getId());
+        assertEquals(dadosProdutos().getDescricao(), response.get(0).getDescricao());
+        assertEquals(dadosProdutos().getQuantidade(), response.get(0).getQuantidade());
+        assertEquals(dadosProdutos().getQuantidade_minima(), response.get(0).getQuantidade_minima());
+        assertEquals(dadosProdutos().getPreco_compra(), response.get(0).getPreco_compra());
+        assertEquals(dadosProdutos().getPreco_venda(), response.get(0).getPreco_venda());
+        assertEquals(dadosProdutos().getCodigo_barras(), response.get(0).getCodigo_barras());
+    }
+
+    @DisplayName("Pesquisa a descrição dos produtos por IDs")
+    @Test
+    void testPesquisaDescricaoProdutos(){
+        when(produtoRepository.findAllById(dadosIds())).thenReturn(listModelProdutos);
+
+        List<String> response = utilProdutos.pesquisaDescricaoProdutosPorIds(dadosIds());
+
+        assertNotNull(response);
+
+        assertEquals(dadosDescricaoProdutos().get(0), response.get(0));
+    }
+
+    @DisplayName("Teste para somar o Estoque atual dos Produtos para o Dashboard")
+    @Test
+    void testsomaEstoqueAtualProdutos(){
+        when(produtoRepository.somaEstoqueAtual()).
+                thenReturn(dadosDashboard().getQuantidadeProdutosEstoqueAtual());
+
+        Optional<Integer> response = utilProdutos.somaEstoqueAtualProdutos();
+
+        assertNotNull(response);
+        assertTrue(response.isPresent());
+        assertEquals(response, dadosDashboard().getQuantidadeProdutosEstoqueAtual());
+    }
+
+    @DisplayName("Teste para retornar NullPointerException caso não tenha dados")
+    @Test
+    void testsomaEstoqueAtualProdutosNullPointerException() {
+        when(produtoRepository.somaEstoqueAtual()).thenThrow(new NullPointerException());
+
+        try {
+            var response = utilProdutos.somaEstoqueAtualProdutos();
+        }catch (NullPointerException exception){
+            assertEquals(NullPointerException.class, exception.getClass());
+        }
+    }
+
     private void startProdutoOptional(){
         modelProdutosOptional = Optional.of(new ModelProdutos(
                 dadosProdutos().getId(),
@@ -78,5 +136,8 @@ class UtilProdutosTest extends ConfigDadosEstaticosEntidades{
                 dadosProdutos().getPreco_venda(),
                 dadosProdutos().getCodigo_barras()
         ));
+        listModelProdutos = Arrays.asList(
+                dadosProdutos()
+        );
     }
 }
