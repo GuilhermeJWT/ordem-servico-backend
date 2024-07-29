0f8d6b3b0a9dca7a7e830293186fd5fdd4198013
GuilhermeJWT <guiromanno@gmail.com>
2024-07-29 10:13:42 -0300

    ADD: Job para verificar Contas a Receber Vencidas

---
+++ src/main/java/br/com/systemsgs/ordem_servico_backend/scheduled/ContasReceberExpiradasScheduled.java
@@ -0,0 +1,34 @@
+package br.com.systemsgs.ordem_servico_backend.scheduled;
+
+import br.com.systemsgs.ordem_servico_backend.enums.StatusContas;
+import br.com.systemsgs.ordem_servico_backend.model.ModelContasReceber;
+import br.com.systemsgs.ordem_servico_backend.repository.ContasReceberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ContasReceberExpiradasScheduled {
+
+    @Autowired
+    private ContasReceberRepository contasReceberRepository;
+
+     //Todo dia as 7 da manhã, o Job será executado para listar as Contas a Receber vencida, e atualizar para = EXPIRADA
+    @Scheduled(cron = "0 0 7 * * *")
+    public void verificaContasReceberVencidas() {
+        contasReceberExpiradas();
+    }
+
+    public void contasReceberExpiradas(){
+        List<ModelContasReceber> contasReceberVencidasHoje = contasReceberRepository.pesquisaContasReceberExpiradas();
+
+        if(!contasReceberVencidasHoje.isEmpty()){
+            contasReceberVencidasHoje.stream()
+                    .forEach(status -> status.setStatusContasReceber(StatusContas.EXPIRADA));
+
+            contasReceberRepository.saveAll(contasReceberVencidasHoje);
+        }
+    }
+}
