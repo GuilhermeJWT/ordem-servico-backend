8f0092937742a5c4ddc1dd3c0bd9a245663eecca
GuilhermeJWT <guiromanno@gmail.com>
2024-09-15 09:47:07 -0300

    ADD: Classe de configuração do Spring Security

---
+++ src/main/java/br/com/systemsgs/ordem_servico_backend/security/EnableWebSecurityConfiguration.java
@@ -0,0 +1,56 @@
+package br.com.systemsgs.ordem_servico_backend.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class EnableWebSecurityConfiguration {
+
+    public static final String[] ROTAS_PUBLICAS = {
+            "/api/usuarios/v1/salvar",
+            "/api/usuarios/v1/login",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-ui.html"
+    };
+
+    private final UserAuthenticationFilter userAuthenticationFilter;
+
+    @Autowired
+    public EnableWebSecurityConfiguration(UserAuthenticationFilter userAuthenticationFilter) {
+        this.userAuthenticationFilter = userAuthenticationFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeHttpRequests()
+                .requestMatchers(ROTAS_PUBLICAS).permitAll()
+                .anyRequest().authenticated()
+                .and().
+                addFilterBefore(userAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
